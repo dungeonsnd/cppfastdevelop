de7bcaae4adb1ca2935f4ba86803dac2d522fcf8
jeffery <dungeonsnd@gmail.com>
2013-06-26 23:05:34 +0800

    add cf_socket

---
+++ cppfoundation/include/cppfoundation/cf_socket.hpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013, Jeffery Qiu. All rights reserved.
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE(the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.gnu.org/licenses/lgpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//// Author: Jeffery Qiu (dungeonsnd at gmail dot com)
+////
+
+#ifndef _HEADER_FILE_CFD_CF_SOCKET_HPP_
+#define _HEADER_FILE_CFD_CF_SOCKET_HPP_
+
+#include "cppfoundation/cf_root.hpp"
+#include "cppfoundation/cf_exception.hpp"
+
+namespace cf
+{
+
+namespace socketdefs
+{
+enum
+{
+    DEFAULT_STACKSIZE =1024*1024*2
+};
+} // namespace socketdefs
+
+class Socket:NonCopyable
+{
+public:
+    Socket() {}
+    virtual ~Socket() {}
+} // namespace cf
+
+#endif // _HEADER_FILE_CFD_CF_SOCKET_HPP_
